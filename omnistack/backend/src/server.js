7712a5b78352e51519b1e31e3313b2d3ff11639e
Antonio Carlos Gomes <acgomes68@gmail.com>
2019-10-01 13:36:32 -0300

    Instalação do nodemon, construção das primeiras rotas do tipo GET

--- omnistack/backend/src/server.js
+++ omnistack/backend/src/server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json({message: "Hello brave new world"});
+});
+
+app.post('/users', (req, res) => {
+    return res.json({user: req.post.user});
+});
+
+app.get('/contacts', (req, res) => {
+    return res.json({contact: req.query.contact});
+});
+
+app.listen(3333);
