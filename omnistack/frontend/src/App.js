29ffc26ed61e7e8ee2d53d5986acb58419a2923d
Antonio Carlos Gomes <acgomes68@gmail.com>
2019-10-03 13:49:30 -0300

    Integração da biblioteca react-router-dom, cadastramento das informaçẽos do usuário da sessão chamando o backend através do frontend

--- omnistack/frontend/src/App.js
+++ omnistack/frontend/src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
+import api from './services/api';
 
 
 function App() {
+  const [ email, setEmail ] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/users', { email });
+
+    console.log(response);
+
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
 
       <div className="content">
         <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">e-Mail(*)</label>
           <input 
             type="email" 
             id="email" 
+            value={email}
             placeholder="Seu melhor e-mail"
+            onChange={event => setEmail(event.target.value)}
             />
             <button type="submit" className="btn">Entrar</button>
         </form>
