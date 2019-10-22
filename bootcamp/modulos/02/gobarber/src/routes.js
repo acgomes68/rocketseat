3ebbeb52c0595938a339281c763c5a8135b31bd9
Antonio Carlos Gomes <acgomes68@gmail.com>
2019-10-22 02:13:26 -0300

    Instalação do nodemon e sucrase, configuração para debug, substituição do "const...require" para "import ...from" no código dos arquivos

---
+++ bootcamp/modulos/02/gobarber/src/routes.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+    return res.json({ hello: 'World' });
+});
+
+routes.get('/:id', (req, res) => {
+    return res.json({ hello: 'World' });
+});
+
+routes.post('/', (req, res) => {
+    return res.json({ hello: 'World' });
+});
+
+routes.put('/:id', (req, res) => {
+    return res.json({ hello: 'World' });
+});
+
+routes.delete('/:id', (req, res) => {
+    return res.json({ hello: 'World' });
+});
+
+export default routes;
