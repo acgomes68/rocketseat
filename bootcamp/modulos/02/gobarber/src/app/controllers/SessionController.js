aaced9a662d7154b2be38d61aa9c1fcf910b8155
Antonio Carlos Gomes <acgomes68@gmail.com>
2019-10-22 08:18:23 -0300

    Instalação do JWT, criação do SessionController

---
+++ bootcamp/modulos/02/gobarber/src/app/controllers/SessionController.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        nome,
+        email,
+      },
+      token: jwt.sign({ id }),
+    });
+  }
+}
+export default new SessionController();
