72167962fd7282347a42487d805e183c9a73cde9
Antonio Carlos Gomes <acgomes68@gmail.com>
2019-10-29 06:08:05 -0300

    Criação das rotas de pedidos de auxílio, checkins, planos e matrículas

--- bootcamp/desafios/03/gympoint/src/routes.js
+++ bootcamp/desafios/03/gympoint/src/routes.js
@@ -10,11 +10,13 @@ const routes = new Router();
 
 // Public access routes
 
+// Sessions
+routes.post('/sessions', SessionController.store);
+
 // Users
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.store);
-routes.put('/users/', UserController.update);
 routes.delete('/users/:id', UserController.delete);
 
 // Students
@@ -24,8 +26,31 @@ routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
 routes.delete('/students/:id', StudentController.delete);
 
-// Sessions
-routes.post('/sessions', SessionController.store);
+// Students Checkins
+routes.post('/students/:id/checkins', CheckinController.store);
+routes.get('/students/:id/checkins', CheckinController.show);
+
+// Students Help Orders
+routes.post('/students/:id/help-orders', HelpOrderController.store);
+routes.get('/students/:id/help-orders', HelpOrderController.show);
+
+// Plans
+routes.get('/plans', PlanController.index);
+routes.get('/plans/:id', PlanController.show);
+routes.post('/plans', PlanController.store);
+routes.put('/plans/:id', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
+
+// Registrations
+routes.get('/registrations', RegistrationController.index);
+routes.get('/registrations/:id', RegistrationController.show);
+routes.post('/registrations', RegistrationController.store);
+routes.put('/registrations/:id', RegistrationController.update);
+routes.delete('/registrations/:id', RegistrationController.delete);
+
+// Help Orders
+routes.get('/help-orders/noanswer', HelpOrderController.show);
+routes.post('/help-orders/:id/answer', HelpOrderController.store);
 
 // Restricted access routes
 routes.use(authMiddlewware);
