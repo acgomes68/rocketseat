20bd0ae127d0038c06837475ed47220492e094e1
Antonio Carlos Gomes <acgomes68@gmail.com>
2019-11-08 19:33:16 -0300

    Instalação e configuração do nodemailer, cadastramento e configuração do Mailtrap, criação da lib e config  de e-mail, envio de e-mail no cancelamento do agendamento

--- bootcamp/modulos/03/gobarber/src/config/environment.js
+++ bootcamp/modulos/03/gobarber/src/config/environment.js
@@ -7,10 +7,10 @@ dotenv.config({
 const { APP_NAME, APP_URL, APP_PORT, APP_HOST } = process.env;
 
 // export default {
-//   APP_NAME: {APP_NAME},
-//   APP_URL: {APP_URL},
-//   APP_PORT: {APP_PORT},
-//   APP_HOST: {APP_HOST},
+//   APP_NAME: { APP_NAME },
+//   APP_URL: { APP_URL },
+//   APP_PORT: { APP_PORT },
+//   APP_HOST: { APP_HOST },
 // };
 module.exports = {
   APP_NAME,
---
+++ bootcamp/modulos/03/gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '87e636c9ca860f',
+    pass: '666b868ffe6b47',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
